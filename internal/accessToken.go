36d1facbd581043646cf4b39e18ec56304bfcb67
Antoine Van Luchem <antoine.vanluchem@student.kuleuven.be>
2021-03-27 15:09:37 +0100

    accessToken v1

--- internal/accessToken.go
+++ internal/accessToken.go
@@ -5,24 +5,19 @@ import (
 	"os"
 )
 
-// A struct to save the accessToken
-type AccessToken struct {
-	Token string
-}
-
 // Function to read the access token at a specified path
 // If no path is provided (empty string), an empty access token is returned
-func ReadTokenFile(path string) (AccessToken, error) {
+func ReadTokenFile(path string) (string, error) {
 
-	if path == "" {
-		return AccessToken{}, nil
-	}
+	// if path == "" {
+	// 	return AccessToken{}, nil
+	// }
 
 	accessToken, err := os.ReadFile(path)
 	if err != nil {
-		return AccessToken{}, errors.New("something went wrong when reading the file")
+		return "", errors.New("something went wrong when reading the file")
 	}
 
-	return AccessToken{Token: string(accessToken)}, nil
+	return string(accessToken), nil
 
 }
