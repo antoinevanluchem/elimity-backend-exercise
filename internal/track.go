5840a6820e595256836092823b9dd65ff6589147
Antoine Van Luchem <antoine.vanluchem@student.kuleuven.be>
2021-03-27 21:32:10 +0100

    finished

--- internal/track.go
+++ internal/track.go
@@ -24,17 +24,12 @@ type TrackOptions struct {
 // Track tracks public GitHub repositories, optional parameters must be given via a TrackOptions struct.
 func Track(trackOptions TrackOptions) error {
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 0, ' ', tabwriter.Debug)
 	headers := "Owner\tName\tUpdated at (UTC)\tStar count"
 	fmt.Fprintln(w, headers)
 
-	// headers := []string{"Owner", "Name", "Updated at (UTC)", "Star count"}
-	// pPrinter := NewPrettyPrinter(headers, " ", " |")
-
 	client := trackOptions.Client.GithubClient
 	con := trackOptions.Client.Context
-
-	// i := 0
 
 	for ; ; <-time.Tick(trackOptions.Interval) {
 
@@ -57,27 +52,8 @@ func Track(trackOptions TrackOptions) error {
 
 			row := fmt.Sprintf("%s\t"+"%s\t"+"%s\t"+"%d", ownerName, repoName, updatedAt, stars)
 			fmt.Fprintln(w, row)
-			// row := map[string]string{"Owner": ownerName, "Name": repoName, "Updated at (UTC)": updatedAt, "Star count": strconv.Itoa(stars)}
-
-			// pPrinter, err = pPrinter.AddRow(row)
-			// if err != nil {
-			// 	return err
-			// }
-
 		}
 
 		w.Flush()
-
-		// if i == 0 {
-		// 	pPrinter.Print()
-		// } else {
-		// 	err := pPrinter.PrintLastNRows(len(result.Repositories))
-		// 	if err != nil {
-		// 		return err
-		// 	}
-		// }
-
-		// i++
-
 	}
 }
